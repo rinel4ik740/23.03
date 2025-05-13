c88e534e1f644473690d0a875d5005091877d00b
rinel4ik740 <rzinnatov@litsey7.com>
2025-05-13 20:19:39 +0300

    Add files via upload

---
+++ Program3.1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13._03._2025
+{
+    class ex3
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+            stack.Push(5);
+            foreach (var item in stack)
+            {
+                Console.Write(item);
+            }
+        }
+    }
+}
---
+++ Program5.1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    class ex5
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<char, char> dict = new Dictionary<char, char>();
+            dict.Add('(', ')');
+            dict.Add('[', ']');
+            dict.Add('{', '}');
+
+            Stack<char> stack = new Stack<char>();
+
+            #region заполнение_стека
+            Console.WriteLine("Введите размер массива: ");
+
+
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите последовательность из скобок: ");
+
+            for (int i = 0; i < a; i++)
+            {
+                stack.Push(char.Parse(Console.ReadLine()));
+            }
+            #endregion
+
+            char[] arr = stack.ToArray();
+
+            if (isCorrect(arr, dict) == true)
+            { 
+                Console.WriteLine("Correct");
+            }
+            else Console.WriteLine("Uncorrect");
+        }
+
+        public static bool isCorrect(char[] arr, Dictionary<char, char> dict)
+        {
+            int count = arr.Length;
+            bool isCorrect = true;
+            if (count % 2 == 0)
+            {
+                for (int i = 0; i < count / 2; i++)
+                {
+                    if ((((arr[i] == dict.Values.ElementAt(0)) && arr[count - 1 - i] == dict.Keys.ElementAt(0)) ||
+                        ((arr[i] == dict.Values.ElementAt(1)) && arr[count - 1 - i] == dict.Keys.ElementAt(1)) ||
+                        ((arr[i] == dict.Values.ElementAt(2)) && arr[count - 1 - i] == dict.Keys.ElementAt(2))) && (isCorrect == true))
+                    {
+
+                    }
+                    else isCorrect = false;
+                }
+                return isCorrect;
+            }
+            else return false;
+        }
+    }
+}
